47be011dfd48e35e1a0735d89053cf2abd08251d
itsXrgon <ali.koheil@gmail.com>
2024-05-04 15:35:40 +0300

    feat: details add edit validation done

--- src/Components/details/Details.tsx
+++ src/Components/details/Details.tsx
@@ -1,31 +1,27 @@
 import { useDetailsContext } from './useDetailsContext';
 import { Button } from '../shadcn/ui/button';
 import { useFormContext } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 export default function Details({ children }: React.PropsWithChildren) {
 	const { isAddMode, isEditMode, setIsAddMode, setIsEditMode, title, id } =
 		useDetailsContext();
 
 	const { handleSubmit } = useFormContext();
+	const navigate = useNavigate();
 
-	const handleSave = () => {
-		handleSubmit((data) => {
-			console.log(data);
-			setIsEditMode(false);
-		});
-	};
-
-	const handleEdit = () => {
-		setIsEditMode(true);
-	};
-
-	const handleAdd = () => {
-		handleSubmit(() => {
+	const onSubmit = (_: any) => {
+		if (isAddMode) {
 			setIsAddMode(false);
-		});
+			navigate(`..`);
+		} else if (isEditMode) {
+			setIsEditMode(false);
+		} else {
+			setIsEditMode(true);
+		}
 	};
 
 	return (
-		<>
+		<form onSubmit={handleSubmit(onSubmit)}>
 			<div className="h-12 w-full flex justify-between px-12 items-center">
 				<h1 className="text-3xl font-bold text-black">
 					{isAddMode
@@ -35,23 +31,12 @@ export default function Details({ children }: React.PropsWithChildren) {
 							: ` ${title} - ${id} Details`}
 				</h1>
 				<div className="flex items-center gap-4">
-					<Button
-						className="text-white w-32 h-10 text-lg"
-						onClick={() => {
-							if (isAddMode) {
-								handleAdd();
-							} else if (isEditMode) {
-								handleSave();
-							} else {
-								handleEdit();
-							}
-						}}
-					>
+					<Button className="text-white w-32 h-10 text-lg" type="submit">
 						{isAddMode ? 'Add' : isEditMode ? 'Save' : 'Edit'}
 					</Button>
 				</div>
 			</div>
 			{children}
-		</>
+		</form>
 	);
 }
