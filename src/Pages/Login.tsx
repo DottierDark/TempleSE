e6c7a80f087a0ecb0d8226693979722a61581351
Nasseryoyo <youussefnasser@gmail.com>
2024-05-10 14:36:52 +0300

    fix:topbar showing without refresh

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import {
 	Form,
 	FormControl,
@@ -6,59 +6,60 @@ import {
 	FormItem,
 	FormLabel,
 	FormMessage,
-} from '../Components/shadcn/ui/form';
-import { Input } from '../Components/shadcn/ui/input';
-import { z } from 'zod';
-import { Button } from '../Components/shadcn/ui/button';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { Card } from '../Components/shadcn/ui/card';
-import { credentials } from '../assets/dummyData';
-import { User } from '../types';
+} from "../Components/shadcn/ui/form";
+import { Input } from "../Components/shadcn/ui/input";
+import { z } from "zod";
+import { Button } from "../Components/shadcn/ui/button";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Card } from "../Components/shadcn/ui/card";
+import { credentials } from "../assets/dummyData";
+import { User } from "../types";
 
 export default function Login() {
 	const navigate = useNavigate();
 	const formSchema = z.object({
 		username: z.string().min(2, {
-			message: 'Username must be at least 3 characters.',
+			message: "Username must be at least 3 characters.",
 		}),
 		password: z.string().min(5, {
-			message: 'Password must be at least 6 characters.',
+			message: "Password must be at least 6 characters.",
 		}),
 	});
 
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
 		defaultValues: {
-			username: '',
-			password: '',
+			username: "",
+			password: "",
 		},
 	});
 
 	function onSubmit(values: z.infer<typeof formSchema>) {
 		const user = credentials.find(
 			(cred: User) =>
-				cred.username === values.username && cred.password === values.password
+				cred.username === values.username &&
+				cred.password === values.password,
 		);
 
 		if (user) {
 			navigate(`/${user.type}`); // Navigate based on user type
-			localStorage.setItem('user', JSON.stringify(user));
+			localStorage.setItem("user", JSON.stringify(user));
 		} else {
-			alert('Invalid username or password.');
+			alert("Invalid username or password.");
 		}
 	}
 
 	return (
 		<div className="absolute flex h-full w-full flex-col items-center justify-center bg-gray-900 ">
-			<Card className="border border-gray-700 bg-gray-800 p-12 flex flex-col gap-5 rounded-2xl h-[500px] w-[500px] text-white">
+			<Card className="flex h-[500px] w-[500px] flex-col gap-5 rounded-2xl border border-gray-700 bg-gray-800 p-12 text-white">
 				<div className="flex w-full flex-col items-center justify-center gap-5">
 					<div className="text-3xl font-bold">Login</div>
 					<div className="flex w-full items-center justify-center gap-1 text-lg">
 						<div className="">Don't have an account?</div>
 						<a
-							className=" text-primary-500 cursor-pointer font-medium underline hover:text-primary-400"
-							onClick={() => navigate('/mainregister')}
+							className=" text-primary-500 hover:text-primary-400 cursor-pointer font-medium underline"
+							onClick={() => navigate("/register")}
 						>
 							Sign up.
 						</a>
@@ -76,7 +77,10 @@ export default function Login() {
 								<FormItem className="h-[100px]">
 									<FormLabel>Username</FormLabel>
 									<FormControl>
-										<Input placeholder="ahmed123" {...field} />
+										<Input
+											placeholder="ahmed123"
+											{...field}
+										/>
 									</FormControl>
 									<FormMessage />
 								</FormItem>
@@ -89,7 +93,10 @@ export default function Login() {
 								<FormItem className="h-[100px]">
 									<FormLabel>Password</FormLabel>
 									<FormControl>
-										<Input placeholder="password123" {...field} />
+										<Input
+											placeholder="password123"
+											{...field}
+										/>
 									</FormControl>
 									<FormMessage />
 								</FormItem>
@@ -99,7 +106,7 @@ export default function Login() {
 							type="submit"
 							variant="default"
 							size="lg"
-							className="text-lg mt-5"
+							className="mt-5 text-lg"
 						>
 							Login
 						</Button>
