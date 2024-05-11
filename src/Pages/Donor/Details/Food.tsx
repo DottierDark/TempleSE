e768356e8e4f0c15782ef392d7105df8daf9d1d0
itsXrgon <ali.koheil@gmail.com>
2024-05-11 22:33:53 +0300

    feat: clothes and food details pages

--- src/Pages/Donor/Details/Food.tsx
+++ src/Pages/Donor/Details/Food.tsx
@@ -1,21 +1,84 @@
-import React from 'react';
+import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
 import { foods } from '../../../assets/dummyData';
+import { DetailsContextProvider } from '../../../Components/details/useDetailsContext';
+import { TFoodItem } from '../../../types';
+import { Form } from '../../../Components/shadcn/ui/form';
+import { useEffect } from 'react';
+import Details from '../../../Components/details/Details';
+import TextField from '../../../Components/details/fields/TextField';
+import NumberField from '../../../Components/details/fields/NumberField';
+import ImageField from '../../../Components/details/fields/ImageField';
 
 export default function Food() {
 	const { id } = useParams<{ id: string }>();
-	const food = foods.find((item) => item.id.toString() === id);
-	if (!food) {
-		return <div>Food not found!</div>;
-	}
+	const food = foods.find((item) => item.id.toString() === id) as TFoodItem;
+
+	const form = useForm<TFoodItem>({
+		defaultValues: {
+			id: undefined,
+			name: '',
+			type: 'fresh',
+			img: '',
+			quantity: 0,
+		},
+	});
+
+	useEffect(() => {
+		if (food) {
+			form.reset(food);
+		}
+	}, [food]);
+
+	const foodType = form.watch('type');
+
+	const typeUnit = {
+		fresh: 'kg',
+		canned: 'liters',
+		baked: 'items',
+		'fruits and vegetables': 'kg',
+	};
+
 	return (
-		<div>
-			<h1>{food.name}</h1>
-			<p>
-				Quantity: {food.quantity}{' '}
-				{food.type === 'fruits and vegtables' ? 'KG' : 'Amount'}
-			</p>
-			<p>Type: {food.type}</p>
-		</div>
+		<Form {...form}>
+			<DetailsContextProvider title="Clothes item" id={id} canEdit={false}>
+				<Details>
+					<div className="grid grid-cols-2 gap-5 p-6">
+						<TextField
+							name="name"
+							label="Name"
+							description="Name of the food"
+							detailsClassName=""
+							inputClassName=""
+							placeholder=""
+						/>
+						<TextField
+							name="type"
+							label="Type"
+							description="Type of the food"
+							detailsClassName=""
+							inputClassName=""
+							placeholder=""
+						/>
+						<NumberField
+							name="quantity"
+							label={`Quantity (${typeUnit[foodType as keyof typeof typeUnit]})`}
+							description={`Quantity of the clothes item in ${typeUnit[foodType as keyof typeof typeUnit]}`}
+							detailsClassName=""
+							inputClassName=""
+							placeholder=""
+						/>
+						<ImageField
+							name="img"
+							label="Image"
+							description="Image of the food item"
+							detailsClassName=""
+							inputClassName=""
+							placeholder=""
+						/>
+					</div>
+				</Details>
+			</DetailsContextProvider>
+		</Form>
 	);
 }
